0757a650982d3d4a5e196b044046e0ba5684a27c
Aluisio Penna <aluisioordones1@gmail.com>
2023-08-09 21:42:24 -0300

    altera titulo desenvolvedores de software para quem somos?; aumenta tamanho das letras de quem somos; altera texto no footer; adiciona hr entre o footer e main; retira camel case do titulo central e outras mudancas associadas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,14 @@ export default function Home() {
       <Header />
       <Presentation />
       <section className="font-preahvihear ml-32 w-3/5 flex flex-col gap-10">
-        <h1 className="text-4xl">Desenvolvedores de Software</h1>
-        <p className="text-sm leading-8 indent-8">
-        Desbravando o futuro digital: bem-vindo(a) a Devians Tech, 
-        onde a criatividade encontra a tecnologia e sua visão se torna parte da web. 
-        Somos mais que uma empresa de desenvolvimento web, somos artesãos digitais 
-        apaixonados por criar experiências virtuais excepcionais. Prezamos pela 
-        qualidade de nossas entregas com responsabilidade e respeito aos nossos 
-        clientes.
+        <h1 className="text-4xl">Quem somos?</h1>
+        <p className="text-lg leading-8 indent-8">
+          Desbravando o futuro digital: bem-vindo(a) a Devians Tech, 
+          onde a criatividade encontra a tecnologia e sua visão se torna parte da web. 
+          Somos mais que uma empresa de desenvolvimento web, somos artesãos digitais 
+          apaixonados por criar experiências virtuais excepcionais. Prezamos pela 
+          qualidade de nossas entregas com responsabilidade e respeito aos nossos 
+          clientes.
         </p>
       </section>
       <Footer />
